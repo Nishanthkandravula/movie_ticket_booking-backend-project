868911f149d5fa8d354c9d239bb7e88ceee51a8f
Nishanthkandravula <kandravula.nishant@gmail.com>
2024-12-21 19:30:52 +0530

    Initial commit

---
+++ routes/movieRoutes.js
@@ -0,0 +1,25 @@
+
+const router = express.Router();
+const { movie } = require('../models');
+
+// Get all movie
+router.get('/', async (req, res) => {
+  try {
+    const movie = await movie.findAll();
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Add a new movie
+router.post('/', async (req, res) => {
+  try {
+    const movie = await movie.create(req.body);
+    res.status(201).json(movie);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
